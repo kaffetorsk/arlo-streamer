f960a3c41023f2620745a0808243b47db0526a70
Kaffetorsk <93540369+kaffetorsk@users.noreply.github.com>
2022-11-02 13:32:48 +0100

    major refactor

--- camera.py
+++ camera.py
@@ -4,35 +4,83 @@ import asyncio
 import shlex
 import os
 
+
 class Camera(object):
     STATES = ['idle', 'connecting', 'streaming']
 
-    @classmethod
-    async def create(cls, arlo_camera, ffmpeg_out, motion_timeout):
-        self = Camera()
+    def __init__(self, arlo_camera, ffmpeg_out, motion_timeout):
         self._arlo = arlo_camera
         self.name = self._arlo.name.replace(" ", "_")
         self.ffmpeg_out = shlex.split(ffmpeg_out.format(name=self.name))
         self.timeout = motion_timeout
-        self.event_loop = asyncio.get_running_loop()
-        self._arlo.add_attr_callback('*', self._attribute_changed)
-        self._state = 'idle'
-        self.proxy_stream, self.proxy_writer = await self._start_proxy_stream()
-        self.stream = await self._start_idle_stream()
         self.timeout_task = None
-        self.callbacks = {}
+        self._state = None
+        self.stream = None
+        self.pictures = asyncio.Queue()
+        self._listen_pictures = False
         logging.info(f"Camera added: {self.name}")
-        return self
 
-    @property
-    def state(self):
-        return self._state
+    async def run(self):
+        self.event_loop = asyncio.get_running_loop()
+        event_get, event_put = self.make_iter()
+        self._arlo.add_attr_callback('*', event_put)
+        self.proxy_stream, self.proxy_writer = await self._start_proxy_stream()
+        await self.set_state('idle')
 
-    @state.setter
-    def state(self, new_state):
-        if new_state in self.STATES:
+        async for device, attr, value in event_get:
+            if device == self._arlo:
+                await self.on_event(attr, value)
+
+    async def on_event(self, attr, value):
+        print(attr, value)
+        match attr:
+            case 'motionDetected':
+                await self.on_motion(value)
+            case 'activityState':
+                await self.on_arlo_state(value)
+            case 'presignedLastImageData':
+                await self.pictures.put(value)
+            case _:
+                pass
+
+    async def on_motion(self, motion):
+        logging.info(f"{self.name} motion: {motion}")
+        if motion and self.state == 'idle':
+            await self.set_state('connecting')
+
+        if (not motion) and self.state != 'idle':
+            if self.timeout_task:
+                self.timeout_task.cancel()
+            self.timeout_task = asyncio.create_task(self._stream_timeout)
+            await self.timeout_task
+
+    async def on_arlo_state(self, state):
+        if state == 'idle':
+            if self.state == 'connecting':
+                await self.request_stream()
+            if self.state == 'streaming':
+                await self.set_state('idle')
+        elif state == 'userStreamActive' and self.state != 'streaming':
+            await self._stream_started()
+
+    async def set_state(self, new_state):
+        if new_state in self.STATES and new_state != self._state:
             self._state = new_state
             logging.info(f"{self.name} state: {new_state}")
+            await self._on_state_change(new_state)
+
+    def get_state(self):
+        return self._state
+
+    async def _on_state_change(self, new_state):
+        match new_state:
+            case 'idle':
+                await self.stop_stream()
+                self.stream = await self._start_idle_stream()
+            case 'connecting':
+                await self.request_stream()
+            case 'streaming':
+                pass
 
     async def _start_proxy_stream(self):
         read, write = os.pipe()
@@ -44,11 +92,15 @@ class Camera(object):
 
     async def _start_idle_stream(self):
         proc = await asyncio.create_subprocess_exec(
-            *['ffmpeg', '-re', '-loop', '1', '-f', 'image2', '-i', 'arlo.png',
-            '-r', '30', '-c:v', 'libx264', '-bsf', 'dump_extra', '-f', 'mpegts', 'pipe:'],
+            *['ffmpeg', '-re', '-loop', '1', '-f', 'image2',
+                '-i', 'arlo.png', '-r', '30', '-c:v', 'libx264',
+                '-bsf', 'dump_extra', '-f', 'mpegts', 'pipe:'],
             stdout=self.proxy_writer, stderr=subprocess.DEVNULL
             )
         return proc
+
+    async def request_stream(self):
+        self.event_loop.run_in_executor(None, self._arlo.get_stream)
 
     async def _stream_started(self):
         self.state = 'streaming'
@@ -56,73 +108,54 @@ class Camera(object):
         if stream:
             self.stream.kill()
             self.stream = await asyncio.create_subprocess_exec(
-                *['ffmpeg', '-i', stream, '-c:v', 'copy', '-c:a', 'copy', '-bsf', 'dump_extra', '-f', 'mpegts', 'pipe:'],
+                *['ffmpeg', '-i', stream, '-c:v', 'copy', '-c:a', 'copy',
+                    '-bsf', 'dump_extra', '-f', 'mpegts', 'pipe:'],
                 stdout=self.proxy_writer, stderr=subprocess.DEVNULL
                 )
             rc = await self.stream.wait()
         if (not stream) or (rc != -9):
-            self.state = 'connecting'
-            self.start_stream()
+            await self.set_state('connecting')
 
     async def _stream_timeout(self):
         await asyncio.sleep(self.timeout)
-        await self.stop_stream()
+        await self.set_state('idle')
 
     async def stop_stream(self):
-        self.stream.kill()
+        if self.stream:
+            self.stream.kill()
         self.stream = await self._start_idle_stream()
         try:
-            await self.event_loop.run_in_executor(None, self._arlo.stop_activity)
+            await self.event_loop.run_in_executor(
+                    None, self._arlo.stop_activity)
         except AttributeError:
             pass
 
-    def _attribute_changed(self, device, attr, value):
-        if device == self._arlo:
-            match attr:
-                case 'motionDetected':
-                    logging.info(f"{self.name} motion: {value}")
-                    if value == True and self.state == 'idle':
-                        self.start_stream()
+    async def get_pictures(self):
+        self._listen_pictures = True
+        while True:
+            yield self.name, await self.pictures.get()
 
-                    if value == False and self.state != 'idle':
-                        if self.timeout_task:
-                            self.timeout_task.cancel()
-                        self.timeout_task = asyncio.run_coroutine_threadsafe(self._stream_timeout(), self.event_loop)
+    async def mqtt_control(self, payload):
+        match payload.upper():
+            case 'START':
+                if self.get_state() == 'idle':
+                    await self.set_state('connecting')
+            case 'STOP':
+                await self.set_state('idle')
+            case 'SNAPSHOT':
+                await self.event_loop.run_in_executor(
+                        None, self._arlo.request_snapshot)
 
-                case 'activityState':
-                    if  value == 'idle':
-                        if self.state == 'connecting':
-                            self._arlo.get_stream()
-                        if self.state == 'streaming':
-                            self.state = 'idle'
-                    elif value == 'userStreamActive' and self.state != 'streaming':
-                        asyncio.run_coroutine_threadsafe(self._stream_started(), self.event_loop)
+    def make_iter(self):
+        queue = asyncio.Queue()
 
-                case other:
-                    self._check_callbacks(attr, value)
+        def put(*args):
+            self.event_loop.call_soon_threadsafe(queue.put_nowait, args)
 
-    def start_stream(self):
-        self.state = 'connecting'
-        self._arlo.get_stream()
-
-    def add_picture_callback(self, callback):
-        self._add_callback(callback, 'presignedLastImageData')
-
-    # def add_location_callback(self, callback):
-    #    self._add_callback(callback, 'presignedLastImageData')
-
-
-    def _add_callback(self, callback, event):
-        if event not in self.callbacks:
-            self.callbacks[event] = []
-        self.callbacks[event].append(callback)
-
-    def _check_callbacks(self, attr, value):
-        if attr in self.callbacks:
-            for cb in self.callbacks[attr]:
-                # self.event_loop.call_soon_threadsafe(cb, value)
-                self.event_loop.run_in_executor(None, cb, value, self.name)
-
+        async def get():
+            while True:
+                yield await queue.get()
+        return get(), put
 
     def shutdown(self, signal):
         logging.info(f"Shutting down {self.name}")
