e4734ec20582d074c1d2637f091dfdc42988a8b0
tyr84 <stian.brog@gmail.com>
2023-10-11 21:36:26 +0200

    PEP

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ FFMPEG_OUT = config('FFMPEG_OUT')
 MOTION_TIMEOUT = config('MOTION_TIMEOUT', default=60, cast=int)
 STATUS_INTERVAL = config('STATUS_INTERVAL', default=120, cast=int)
 DEBUG = config('DEBUG', default=False, cast=bool)
-PYAARLO_ECDH_CURVE=config('PYAARLO_ECDH_CURVE', default=None)
-PYAARLO_BACKEND=config('PYAARLO_BACKEND', default=None)
+PYAARLO_ECDH_CURVE = config('PYAARLO_ECDH_CURVE', default=None)
+PYAARLO_BACKEND = config('PYAARLO_BACKEND', default=None)
 
 # Initialize logging
 logging.basicConfig(
@@ -37,12 +37,12 @@ async def main():
         'tfa_type': 'email',
         'tfa_host': IMAP_HOST,
         'tfa_username': IMAP_USER,
-        'tfa_password': IMAP_PASS 
+        'tfa_password': IMAP_PASS
     }
-    
+
     if PYAARLO_ECDH_CURVE:
         arlo_args['ecdh_curve'] = PYAARLO_ECDH_CURVE
-    
+
     if PYAARLO_BACKEND:
         arlo_args['backend'] = PYAARLO_BACKEND
 
